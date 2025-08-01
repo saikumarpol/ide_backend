9691a8543c9272c184de017150598d48a4a5f9cc
saikumarpola <92100989+saikumarpol@users.noreply.github.com>
2025-08-01 11:37:02 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,26 +1,5 @@
 
 
-// const mongoose = require("mongoose");
-
-// const UserSchema = new mongoose.Schema({
-//   rollNo: { type: String, required: true, unique: true },
-//   name: { type: String, required: true },
-//   passwordHash: { type: String, required: true },
-//   solvedProblems: { type: [String], default: [] }, // store problem IDs
-//   problemsSolved: { type: Number, default: 0 },
-//   streak: { type: Number, default: 0 },
-//   lastSolvedDate: { type: Date, default: null },  // used for tie-breaking
-//   solvedHistory: [
-//     {
-//       problemId: String,
-//       date: Date
-//     }
-//   ]
-// });
-
-// module.exports = mongoose.model("User", UserSchema);
-
-
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
@@ -30,7 +9,7 @@ const UserSchema = new mongoose.Schema({
   solvedProblems: { type: [String], default: [] }, // store problem IDs
   problemsSolved: { type: Number, default: 0 },
   streak: { type: Number, default: 0 },
-  lastSolvedDate: { type: Date, default: null },
+  lastSolvedDate: { type: Date, default: null },  // used for tie-breaking
   solvedHistory: [
     {
       problemId: String,
@@ -40,3 +19,4 @@ const UserSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", UserSchema);
+
